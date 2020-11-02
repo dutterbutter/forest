213cf39a6225245c27aa210d02c5e21db45e68bc
Dustin Brickwood <dustinbrickwood204@gmail.com>
2020-11-02 12:35:11 -0600

    wip

--- scripts/build-rust-docs.sh
+++ scripts/build-rust-docs.sh
@@ -5,6 +5,6 @@ git init
 echo '<meta http-equiv="refresh" content="0; url=https://dutterbutter.github.io/forest/forest_vm/index.html">' > index.html
 git add .
 git config --global -l
-git -c user.name='ci' -c user.email='ci' commit -m 'Deploy documentation'
+git -c user.name='dutterbutter' -c user.email='ci' commit -m 'Deploy documentation'
 echo "${GITHUB_TOKEN}"
 git push -f -q https://git:${GITHUB_TOKEN}@github.com/dutterbutter/forest HEAD:gh-pages
